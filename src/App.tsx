089a1f72729cba92c06510f7e46f928789c3decb
Joe Sandmeyer <github.com@unconscious.com>
2023-04-18 19:32:23 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
-
+//
 function App() {
   return (
     <Router basename={"/" }>
@@ -13,5 +13,5 @@ function App() {
     </Router>
   )
 }
-
+//
 export default App;
