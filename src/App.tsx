02f8918a47664f5dcac8c68654d302ccb2ca5b2f
Joe Sandmeyer <github.com@unconscious.com>
2023-04-19 08:52:13 -0400

    Wrap with <ExperienceProvider> tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ExperienceProvider } from '@vatom/experience-sdk';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
 //
 function App() {
   return (
     <Router basename={"/" }>
+      <ExperienceProvider>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
         </Routes>
+      </ExperienceProvider>
     </Router>
   )
 }
