14b53fab505426508da00a86d1dadfb51249f577
Joe Sandmeyer <github.com@unconscious.com>
2023-06-14 14:17:27 -0700

    Apply base path for bisid 3UewHF0ge9

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ExperienceProvider } from '@vatom/experience-sdk';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
 //
 function App() {
   return (
-    <Router basename={"/" }>
-      <ExperienceProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/home" element={<HomePage />}></Route>
-          <Route path="/about" element={<AboutPage />}></Route>
-        </Routes>
-      </ExperienceProvider>
-    </Router>
+		<BrowserRouter basename="/3UewHF0ge9">
+			<ExperienceProvider>
+				<Routes>
+					<Route path="/" element={<HomePage />}></Route>
+					<Route path="/home" element={<HomePage />}></Route>
+					<Route path="/about" element={<AboutPage />}></Route>
+				</Routes>
+			</ExperienceProvider>
+		</BrowserRouter>
   )
 }
 //
